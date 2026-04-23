c62533568ebb20312838bd7f393a3980980af485
PanTs <pants.ath@gmail.com>
2026-04-23 18:11:31 +0300

    feat: update UserInsertDTO validation constraints for username and roleId

--- src/main/java/gr/pants/pro/edu_analysis/dto/UserInsertDTO.java
+++ src/main/java/gr/pants/pro/edu_analysis/dto/UserInsertDTO.java
@@ -1,5 +1,6 @@
 package gr.pants.pro.edu_analysis.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -7,14 +8,14 @@ import jakarta.validation.constraints.Size;
 public record UserInsertDTO(
 
         @NotNull
-        @Size(min = 2, max = 20)
+        @Size(min = 3, max = 20)
         String username,
 
         @NotNull
         @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$")
         String password,
 
-        @NotNull
+        @NotBlank
         Long roleId
 ) {
 }
