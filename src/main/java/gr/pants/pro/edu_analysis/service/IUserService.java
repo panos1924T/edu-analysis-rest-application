535f2a2deb832cb56b1d85bfcabb22340ace1bb0
PanTs <pants.ath@gmail.com>
2026-04-23 19:35:48 +0300

    feat: initialize IUserService interface and UserService class for save user

---
+++ src/main/java/gr/pants/pro/edu_analysis/service/IUserService.java
@@ -0,0 +1,21 @@
+package gr.pants.pro.edu_analysis.service;
+
+import gr.pants.pro.edu_analysis.core.exceptions.EntityAlreadyExistsException;
+import gr.pants.pro.edu_analysis.core.exceptions.EntityInvalidArgumentException;
+import gr.pants.pro.edu_analysis.core.exceptions.EntityNotFoundException;
+import gr.pants.pro.edu_analysis.dto.UserInsertDTO;
+import gr.pants.pro.edu_analysis.dto.UserReadOnlyDTO;
+
+import java.util.UUID;
+
+public interface IUserService {
+
+    UserReadOnlyDTO saveUser(UserInsertDTO userInsertDTO)
+            throws EntityAlreadyExistsException, EntityInvalidArgumentException;
+
+    UserReadOnlyDTO getUserByUuid(UUID uuid)
+            throws EntityNotFoundException;
+
+    UserReadOnlyDTO getUserByDeletedFalse(UUID uuid)
+            throws EntityNotFoundException;
+}
---
+++ src/main/java/gr/pants/pro/edu_analysis/service/UserService.java
@@ -0,0 +1,69 @@
+package gr.pants.pro.edu_analysis.service;
+
+import gr.pants.pro.edu_analysis.core.exceptions.EntityAlreadyExistsException;
+import gr.pants.pro.edu_analysis.core.exceptions.EntityInvalidArgumentException;
+import gr.pants.pro.edu_analysis.core.exceptions.EntityNotFoundException;
+import gr.pants.pro.edu_analysis.dto.UserInsertDTO;
+import gr.pants.pro.edu_analysis.dto.UserReadOnlyDTO;
+import gr.pants.pro.edu_analysis.mapper.Mapper;
+import gr.pants.pro.edu_analysis.model.Role;
+import gr.pants.pro.edu_analysis.model.User;
+import gr.pants.pro.edu_analysis.repository.RoleRepository;
+import gr.pants.pro.edu_analysis.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@Slf4j //TODO add logging
+@RequiredArgsConstructor
+public class UserService implements IUserService{
+
+    private final UserRepository userRepository;
+    private final Mapper mapper;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional(rollbackFor = {EntityAlreadyExistsException.class, EntityInvalidArgumentException.class})
+    public UserReadOnlyDTO saveUser(UserInsertDTO userInsertDTO) throws EntityAlreadyExistsException, EntityInvalidArgumentException {
+
+        try {
+
+            if (userRepository.findByUsername(userInsertDTO.username()).isPresent()) {
+                throw new EntityAlreadyExistsException("User", "User with username=" + userInsertDTO.username() + " already exists.");
+            }
+            User user = mapper.toUserEntity(userInsertDTO);
+            user.setPassword(passwordEncoder.encode(userInsertDTO.password()));
+
+            Role role = roleRepository.findById(userInsertDTO.roleId())
+                    .orElseThrow(() -> new EntityInvalidArgumentException("Role", "Role with id=" + userInsertDTO.roleId() + " invalid."));
+            role.addUser(user);
+
+            userRepository.save(user);
+            log.info("User with username={} saved successfully!", userInsertDTO.username());
+            return mapper.toReadOnlyDTO(user);
+
+        } catch (EntityAlreadyExistsException e) {
+            log.error("Save failed! User with username={} already exists.", userInsertDTO.username());
+            throw e;
+        } catch (EntityInvalidArgumentException e) {
+            log.error("Save failed! Invalid arguments for user with username={}", userInsertDTO.username());
+            throw e;
+        }
+    }
+
+    @Override
+    public UserReadOnlyDTO getUserByUuid(UUID uuid) throws EntityNotFoundException {
+        return null;
+    }
+
+    @Override
+    public UserReadOnlyDTO getUserByDeletedFalse(UUID uuid) throws EntityNotFoundException {
+        return null;
+    }
+}
