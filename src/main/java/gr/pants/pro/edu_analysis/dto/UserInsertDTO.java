4d7f1c61a01013acb16cf0d19b940217f63fb89e
PanTs <pants.ath@gmail.com>
2026-04-23 18:05:57 +0300

    feat: initialize user dtos for insert and read-only

---
+++ src/main/java/gr/pants/pro/edu_analysis/dto/UserInsertDTO.java
@@ -0,0 +1,20 @@
+package gr.pants.pro.edu_analysis.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserInsertDTO(
+
+        @NotNull
+        @Size(min = 2, max = 20)
+        String username,
+
+        @NotNull
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$")
+        String password,
+
+        @NotNull
+        Long roleId
+) {
+}
---
+++ src/main/java/gr/pants/pro/edu_analysis/dto/UserReadOnlyDTO.java
@@ -0,0 +1,9 @@
+package gr.pants.pro.edu_analysis.dto;
+
+public record UserReadOnlyDTO(
+
+        String uuid,
+        String username,
+        String role
+) {
+}
