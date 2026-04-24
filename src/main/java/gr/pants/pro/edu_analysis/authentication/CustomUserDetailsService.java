695a04a49f6e70886302b476a536e69a2b75dec2
PanTs <pants.ath@gmail.com>
2026-04-24 18:56:39 +0300

    feat: implement CustomUserDetailsService and JwtService for user authentication

---
+++ src/main/java/gr/pants/pro/edu_analysis/authentication/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package gr.pants.pro.edu_analysis.authentication;
+
+import gr.pants.pro.edu_analysis.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username=" + username + " not found"));
+    }
+}
