00d518d1664cb297409cf850421eca1b7585e7d7
PanTs <pants.ath@gmail.com>
2026-04-26 17:43:42 +0300

    feat: initialize IAnalystService interface for managing analyst data operations

---
+++ src/main/java/gr/pants/pro/edu_analysis/service/IAnalystSevice.java
@@ -0,0 +1,36 @@
+package gr.pants.pro.edu_analysis.service;
+
+import gr.pants.pro.edu_analysis.core.exceptions.EntityAlreadyExistsException;
+import gr.pants.pro.edu_analysis.core.exceptions.EntityInvalidArgumentException;
+import gr.pants.pro.edu_analysis.core.exceptions.EntityNotFoundException;
+import gr.pants.pro.edu_analysis.core.exceptions.FileUploadException;
+import gr.pants.pro.edu_analysis.dto.AnalystInsertDTO;
+import gr.pants.pro.edu_analysis.dto.AnalystReadOnlyDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+public interface IAnalystSevice {
+
+    AnalystReadOnlyDTO saveAnalyst(AnalystInsertDTO insertDTO)
+            throws EntityAlreadyExistsException, EntityInvalidArgumentException;
+
+    void saveIdentityNumberFile(UUID uuid, MultipartFile identityNumberFile)
+            throws FileUploadException, EntityNotFoundException;
+
+//    AnalystReadOnlyDTO updateAnalyst(AnalystUpdateDTO updateDTO)
+//            throws EntityAlreadyExistsException, EntityInvalidArgumentException;
+
+    AnalystReadOnlyDTO deleteAnalystByUuid(UUID uuid)
+            throws EntityNotFoundException;
+
+    AnalystReadOnlyDTO getAnalystByUuid(UUID uuid)
+            throws EntityNotFoundException;
+
+    Page<AnalystReadOnlyDTO> getPaginatedAnalysts(Pageable pageable);
+    Page<AnalystReadOnlyDTO> getPaginatedAnalystsByDeletedFalse(Pageable pageable);
+
+    Boolean isAnalystExists(UUID uuid);
+}
